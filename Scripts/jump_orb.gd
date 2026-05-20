f75e4e755cdcc1c917ee80afac19fa210c337a6d
kimvux <mammuoico12345@gmail.com>
2026-05-20 11:30:02 +0700

    fix movement and animation

--- Scripts/jump_orb.gd
+++ Scripts/jump_orb.gd
@@ -4,4 +4,3 @@ func _on_body_entered(body: Node2D) -> void:
 	print("Da va cham voi: ", body.name)
 	if body.has_method("reset_jump"):
 		body.reset_jump()
-		queue_free()
--- Scripts/main_character.gd
+++ Scripts/main_character.gd
@@ -4,32 +4,30 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -600.0
 const DASH_SPEED = 500.0
 const DASH_DURATION = 0.2
-@onready var sprite_2d = $Sprite2D
 var is_dashing: bool = false
 var dash_direction: float = 1.0
+var input_direction = Vector2.ZERO
 
-var max_jumps: int = 1
+var max_jumps: int = 2
 var jump_count: int = 0
 
 func _physics_process(delta: float) -> void:
+	
 	if is_dashing:
 		move_and_slide()
 		return
-	if (velocity.x > 1 || velocity.x < -1):
-		sprite_2d.animation = "walk"
-	else:
-		sprite_2d.animation = "default"
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		sprite_2d.animation = "jump"
 	else:
 		jump_count = 0
 
 	if Input.is_action_just_pressed("jump") and (is_on_floor() or jump_count < max_jumps):
+		get_node("AnimatedSprite2D").play("idle")
 		velocity.y = JUMP_VELOCITY
 		jump_count += 1
 
 	var direction := Input.get_axis("left", "right")
+	input_direction.x = direction
 	
 	if direction:
 		velocity.x = direction * SPEED
@@ -40,10 +38,8 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("dash"):
 		start_dash()
 
+	update_animation()
 	move_and_slide()
-	
-	var isLeft = velocity.x < 0
-	sprite_2d.flip_h = isLeft
 
 func start_dash() -> void:
 	is_dashing = true
@@ -58,3 +54,20 @@ func end_dash() -> void:
 
 func reset_jump() -> void:
 	jump_count = 0
+	
+func update_animation():
+	var ani = get_node("AnimatedSprite2D")
+
+	if is_on_floor() and input_direction.x != 0:
+		ani.play("run")
+	elif is_on_floor() and input_direction.x == 0:
+		ani.play("idle")
+
+	if input_direction.x > 0:
+		ani.flip_h = false
+	elif input_direction.x < 0:
+		ani.flip_h = true
+
+	if not is_on_floor():
+		if ani.animation != "jump":
+			ani.play("jump")
