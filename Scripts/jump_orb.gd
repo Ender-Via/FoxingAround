b3ea0a0c36a1fe93541d97d1573ce302f768fe82
HkTom021 <lebachquangminh@gmail.com>
2026-05-25 17:28:20 +0700

    UI, map 1

--- Scripts/jump_orb.gd
+++ Scripts/jump_orb.gd
@@ -1,6 +1,18 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-	print("Da va cham voi: ", body.name)
 	if body.has_method("reset_jump"):
 		body.reset_jump()
+		
+		# 1. Ẩn quả cầu đi ngay lập tức
+		visible = false
+		
+		# 2. Khóa vùng va chạm lại để nhân vật không bị ăn đúp khi bay ngang qua
+		$CollisionShape2D.set_deferred("disabled", true)
+		
+		# 3. Chờ đúng 2 giây
+		await get_tree().create_timer(2.0).timeout
+		
+		# 4. Hiện quả cầu lại và mở lại va chạm để dùng cho lượt sau
+		visible = true
+		$CollisionShape2D.set_deferred("disabled", false)
